0e5ea1f2405314ee0be32e51fc2fcf128521af15
Aman Kumar Singh <34673989+amanCoder110599@users.noreply.github.com>
2019-09-26 16:28:10 +0530

    Update DSU.java

--- DSU.java
+++ DSU.java
@@ -1,33 +1,35 @@
 class DSU{
-	int[] par, sz;
-	DSU(int n){
-		par = new int[n];
-		sz = new int[n];
-		int i = 0;
-		for(i = 0; i < n; i++){
-			par[i] = i;
-			sz[i] = i;
-		}
-	}
-	int find(int x){
-		if(par[x] != x)
-			return par[x] = find(par[x]);
-		return par[x];
-	}
-	void unite(int x, int y){
-		int x_root = find(x);
-		int y_root = find(y);
-		if(x_root == y_root)
-			return;
-		Random rand = new Random();
-		int prob = rand.nextInt();
-		if(prob % 2 == 0){
-			par[x_root] = y_root;
-			sz[y_root] += sz[x_root];
-		}
-		else{
-			par[y_root] = x_root;
-			sz[x_root] += sz[y_root];
-		}
-	}
-}
+        int[] par, sz;
+        long ans = 0;
+        DSU(int n){
+            par = new int[n];
+            sz = new int[n];
+            int i = 0;
+            for(i = 0; i < n; i++){
+                par[i] = i;
+                sz[i] = 1;
+            }
+        }
+        int find(int x){
+            if(par[x] != x)
+                return par[x] = find(par[x]);
+            return par[x];
+        }
+        void unite(int x, int y){
+            int x_root = find(x);
+            int y_root = find(y);
+            if(x_root == y_root)
+                return;
+            ans -= nC2(sz[x_root]);
+            ans -= nC2(sz[y_root]);
+            ans += nC2(sz[x_root] + sz[y_root]);
+            if(sz[x_root] <= sz[y_root]){
+                par[x_root] = y_root;
+                sz[y_root] += sz[x_root];
+            }
+            else{
+                par[y_root] = x_root;
+                sz[x_root] += sz[y_root];
+            }
+        }
+    }
